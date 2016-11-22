0749b9a575d8c3fbd6fd5bc0c60ca6cf9ea7b8bb
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-11-22 22:56:53 +0100

    Extended ExprTest3.

--- test/ExprTest3.hlsl
+++ test/ExprTest3.hlsl
@@ -5,6 +5,8 @@
 
 float4 VS() : SV_Position
 {
+	struct A { int z; };
+	typedef A B;
 	typedef int X;
 	int Y = 0;
 	(int) - (1);
@@ -13,5 +15,7 @@ float4 VS() : SV_Position
 	(X) - (1);
 //	((X)) - ((1)); // illegal
 	((Y)) - ((1));
+	(A) - ((1));
+	(B) - ((1));
 	(X)0;
 }
