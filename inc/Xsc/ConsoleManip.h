d915c87d0d01dc7437659e63efd7bc3af734cb94
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-12-04 21:03:45 +0100

    Extended docu.

--- inc/Xsc/ConsoleManip.h
+++ inc/Xsc/ConsoleManip.h
@@ -20,24 +20,24 @@ namespace ConsoleManip
 {
 
 
-//! Console color enumeration.
+//! Output stream color flags enumeration.
 struct ColorFlags
 {
     enum
     {
-        Red     = (1 << 0),
-        Green   = (1 << 1),
-        Blue    = (1 << 2),
+        Red     = (1 << 0),                 //!< Red color flag.
+        Green   = (1 << 1),                 //!< Green color flag.
+        Blue    = (1 << 2),                 //!< Blue color flag.
 
-        Intens  = (1 << 3),
+        Intens  = (1 << 3),                 //!< Intensity color flag.
 
-        Black   = 0,
-        Gray    = (Red | Green | Blue),
-        White   = (Gray | Intens),
+        Black   = 0,                        //!< Black color flag.
+        Gray    = (Red | Green | Blue),     //!< Gray color flag (Red | Green | Blue).
+        White   = (Gray | Intens),          //!< White color flag (Gray | Intens).
 
-        Yellow  = (Red | Green | Intens),
-        Pink    = (Red | Blue | Intens),
-        Cyan    = (Green | Blue | Intens),
+        Yellow  = (Red | Green | Intens),   //!< Yellow color flag (Red | Green | Intens).
+        Pink    = (Red | Blue | Intens),    //!< Pink color flag (Red | Blue | Intens).
+        Cyan    = (Green | Blue | Intens),  //!< Cyan color flag (Green | Blue | Intens).
     };
 };
 
@@ -64,18 +64,37 @@ class ScopedColor
     
     public:
     
+        /**
+        \brief Constructor with output stream and front color flags.
+        \param[in,out] stream Specifies the output stream for which the scope is to be changed. This is only used for Unix systems.
+        \param[in] front Specifies the front color flags. This can be a bitwise OR combination of the entries of the ColorFlags enumeration.
+        \see ColorFlags
+        \see PushColor(std::ostream&, long)
+        */
         inline ScopedColor(std::ostream& stream, long front) :
             stream_{ stream }
         {
             PushColor(stream_, front);
         }
 
+        /**
+        \brief Constructor with output stream, and front- and back color flags.
+        \param[in,out] stream Specifies the output stream for which the scope is to be changed. This is only used for Unix systems.
+        \param[in] front Specifies the front color flags. This can be a bitwise OR combination of the entries of the ColorFlags enumeration.
+        \param[in] back Specifies the back color flags. This can be a bitwise OR combination of the entries of the ColorFlags enumeration.
+        \see ColorFlags
+        \see PushColor(std::ostream&, long, long)
+        */
         inline ScopedColor(std::ostream& stream, long front, long back) :
             stream_{ stream }
         {
             PushColor(stream_, front, back);
         }
 
+        /**
+        \brief Destructor which will reset the previous color from the output stream.
+        \see PopColor
+        */
         inline ~ScopedColor()
         {
             PopColor(stream_);
