d0a07e76572b7e4d8c071f19c704b123824e3a5f
Lukas Hermanns <lukas.hermanns90@gmail.com>
2016-12-17 22:13:35 +0100

    Set version to 0.03-alpha.

--- inc/Xsc/Version.h
+++ inc/Xsc/Version.h
@@ -13,10 +13,10 @@
 #define XSC_VERSION_MAJOR   0
 
 //! Xsc minor version number.
-#define XSC_VERSION_MINOR   2
+#define XSC_VERSION_MINOR   3
 
 //! Xsc version string in the form "X.YZ", where X is the major version, and YZ is the minor version.
-#define XSC_VERSION_STRING  "0.02 Alpha"
+#define XSC_VERSION_STRING  "0.03 Alpha"
 
 
 #endif
