532fa346476376ba3f13f67701461c3ca8d09c8a
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-02-07 11:08:26 +0100

    Added overloaded visitor for "SuffixExpr" and "ArrayAccessExpr" to ConstExprEvaluator (incomplete!).

--- src/Compiler/AST/Visitor/ConstExprEvaluator.h
+++ src/Compiler/AST/Visitor/ConstExprEvaluator.h
@@ -52,6 +52,8 @@ class ConstExprEvaluator : private Visitor
         DECL_VISIT_PROC( PostUnaryExpr    );
         DECL_VISIT_PROC( FunctionCallExpr );
         DECL_VISIT_PROC( BracketExpr      );
+        DECL_VISIT_PROC( SuffixExpr       );
+        DECL_VISIT_PROC( ArrayAccessExpr  );
         DECL_VISIT_PROC( CastExpr         );
         DECL_VISIT_PROC( VarAccessExpr    );
         DECL_VISIT_PROC( InitializerExpr  );
