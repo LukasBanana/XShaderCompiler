3ff9b5dce953f78dd9bfe563909a1117ddd12760
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-02-08 21:59:45 +0100

    Continued with FloatTest2.

--- test/FloatTest2.hlsl
+++ test/FloatTest2.hlsl
@@ -2,7 +2,6 @@
 // HLSL Translator: Floating-point Test 2 (to test expression conversion)
 // 08/02/2017
 
-
 float4 VS(float x : TEXCOORD0) : SV_Position
 {
     float  a = 1 + 1;
@@ -12,5 +11,8 @@ float4 VS(float x : TEXCOORD0) : SV_Position
     float  e = 5;
     float  f = -6 + x;
     float3 g = 3 + 1.5;
+    float3 h = 1 + g + 2;
+    float3 i = h*2.0;
+    float3 j = h*2;
     return 1;
 }
