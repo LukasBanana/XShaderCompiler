9640a2c39d45b0fcefecf5723c14e21f676e1d91
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-03-04 13:46:35 +0100

    Updated IntrinsicTest1.

--- test/IntrinsicTest1.hlsl
+++ test/IntrinsicTest1.hlsl
@@ -2,9 +2,18 @@
 // Intrinsic Test 1
 // 01/03/2017
 
-float4 VS(float3 dir : DIRECTION) : SV_Position
+float4 VS(
+    float3 dir : DIRECTION,
+    float s : SIN,
+    float c : COS) : SV_Position
 {
-    float y = max(1, dir);
-    return y;
+    float a = max(1, dir).z;
+    int b = firstbithigh(3.5);
+    
+    sincos(a, s, c);
+    
+    InterlockedAdd(a, s, c);
+    
+    return a;
 }
 
