aaeec7a810cabc9a9c5147d3202105fce73946bb
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-03-08 16:51:20 +0100

    Added "HalfNxM" data types to GLSL output (converted to "matNxM").

--- test/TypeTest3.hlsl
+++ test/TypeTest3.hlsl
@@ -2,10 +2,6 @@
 // Type Test 3
 // 13/12/2016
 
-//BUG:
-//  parser reads VarDeclStmnt;
-//  type modifiers also not part of "VarType"
-//  which is used for function declarations!
 row_major snorm float4x4 f1(const row_major in snorm float4x4 x)
 {
 	return x;
@@ -15,7 +11,8 @@ float4 VS() : SV_Position
 {
 	snorm float4x4 a = f1(0);
 	unorm half4x4 b = 0;
-	//snorm int c = 0; // ERROR: snorm/unorm only for floating-points
+	unorm double4x4 c = 0;
+	//snorm int d = 0; // ERROR: snorm/unorm only for floating-points
 	
 	
 	return (float4)1;
