60d34b4e280876caf2070b2f2527181d8675d84e
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-03-13 15:12:08 +0100

    Added InitializerTest1 and RWTextureTest1.

---
+++ test/RWTextureTest1.hlsl
@@ -0,0 +1,13 @@
+
+// RWTexture Test 1
+// 13/03/2017
+
+RWTexture2D<int> tex[2];
+
+float4 main() : SV_Position
+{
+    float4 a = (tex[0])[int2(1, 2)]; // must be "imageLoad(tex, ivec2(1, 2))" in GLSL
+    float4 b =  tex[0] [int2(3, 4)]; // must be "imageLoad(tex, ivec2(1, 2))" in GLSL
+    
+    return 1;
+}
