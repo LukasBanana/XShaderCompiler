c3cc3c45649f11deacd01cf74b7f8148b6ac5c50
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-03-30 17:15:45 +0200

    Minor update in RWTextureTest1.

--- test/RWTextureTest1.hlsl
+++ test/RWTextureTest1.hlsl
@@ -6,8 +6,8 @@ RWTexture2D<int> tex[2];
 
 float4 main() : SV_Position
 {
-    float4 a = (tex[0])[int2(1, 2)]; // must be "imageLoad(tex, ivec2(1, 2))" in GLSL
-    float4 b =  tex[0] [int2(3, 4)]; // must be "imageLoad(tex, ivec2(1, 2))" in GLSL
+    float4 a = (tex[0])[int2(1, 2)]; // must be "imageLoad(tex[0], ivec2(1, 2))" in GLSL
+    float4 b =  tex[0] [int2(3, 4)]; // must be "imageLoad(tex[0], ivec2(1, 2))" in GLSL
     
     return 1;
 }
