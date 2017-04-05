b5bf245623b1e0bb264c24f424897f431f412632
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-04-05 22:29:09 +0200

    Updated StructInheritTest3.

--- test/StructInheritTest3.hlsl
+++ test/StructInheritTest3.hlsl
@@ -17,16 +17,24 @@ struct B : A {
 
 struct C : B {
 	int a;
+	int get5() { return A::a; }
+	int get6() { return B::a; }
 };
 
 int A::static_a = 1;
 
-float4 main() : COLOR {
+void main() {
 	C s;
 	s.a = A::static_a;
-	s.C::a = A::a; //WRONG
+	s.C::a = 1;//A::a; //WRONG
 	s.B::a = 2;
 	s.A::a = 3;
 	//s.A::get1();
-	return float4(s.get1(), s.get2(), s.get3(), s.get4());
+	
+	s.get1();
+	s.get2();
+	s.get3();
+	s.get4();
+	s.get5();
+	s.get6();
 }
