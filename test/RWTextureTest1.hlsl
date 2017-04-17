7fb0131e18a7293f803fbda277ca0f34252751c9
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-04-17 19:41:20 +0200

    Simplified RWTextureTest1.

--- test/RWTextureTest1.hlsl
+++ test/RWTextureTest1.hlsl
@@ -4,10 +4,8 @@
 
 RWTexture2D<int> tex[2];
 
-float4 main() : SV_Position
+void main()
 {
     float4 a = (tex[0])[int2(1, 2)]; // must be "imageLoad(tex[0], ivec2(1, 2))" in GLSL
     float4 b =  tex[0] [int2(3, 4)]; // must be "imageLoad(tex[0], ivec2(1, 2))" in GLSL
-    
-    return 1;
 }
