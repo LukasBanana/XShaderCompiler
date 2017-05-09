d3065704ccbf34ba782cfb04fa35a0812875f3b8
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-05-09 20:50:12 +0200

    Extended TexelFetchTest1.

--- test/TexelFetchTest1.hlsl
+++ test/TexelFetchTest1.hlsl
@@ -5,25 +5,28 @@ Texture2D   tex;
 Texture2DMS tex2;
 Texture2D   tex3[2][3];
 
-float4 main() : COLOR
-{
+float4 main() : COLOR {
 	float4 c = 0;
-	
+
 	c += buf[0u];
 	c += buf.Load(0u);
 	c += tex.Load(uint3(0, 0, 0));
 	c += tex2.Load(uint2(0, 0), 0);
-	
+
 	// Access with Operator[]
 	uint3 i = 0;
-	
+	uint2 j = 0;
+
 	c   +=  tex.Load(i);
-	c   +=  tex[i];
+	c   +=  tex[j];
 	c   +=  tex3[1] [2].Load(i);
-	c   += (tex3[1])[2][i];
-	c   +=  tex3[1] [2][i];
-	c.r += (tex3[1] [2][i])[0]; // Single color component
-	c.r +=  tex3[1] [2][i] [0]; // Single color component
-	
+	c   += (tex3[1])[2][j];
+	c   +=  tex3[1] [2][j];
+	c.r += (tex3[1] [2][j])[0]; // Single color component
+	c.r +=  tex3[1] [2][j] [0]; // Single color component
+
+	Texture2D tex4 = tex;
+	Texture2D tex5 = tex3[1][2];
+
 	return c;
 }
