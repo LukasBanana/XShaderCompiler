cd3b60b4012b7273ae8285124579aaaf144487d3
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-05-12 22:41:19 +0200

    Added MatrixMulTest1.

---
+++ test/MatrixMulTest1.hlsl
@@ -0,0 +1,28 @@
+
+// Matrix Mul Test 1
+// 12/05/2017
+
+cbuffer Matrices {
+	float3x3 M3;
+	float4x4 M4;
+	float4x3 M4x3;
+};
+
+void main(float3 v3 : VEC3, float4 v4 : VEC4) {
+	
+	float3x4 a = mul(M3, M4);
+	float4x4 b = mul(M4, M4);
+	float4x4 c = mul(M4x3, a);
+	
+	float3 d = mul(M3, v3);
+	float4 e = mul(M4, v3);
+	float3 f = mul(M3, v4);
+	float4 g = mul(M4, v4);
+	
+	float h = mul(v3, v3);
+	float i = mul(v3, v4);
+	float j = mul(v4, v3);
+	float k = mul(v4, v4);
+	
+}
+
