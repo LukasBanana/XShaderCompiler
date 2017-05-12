214195acc24ed717bf60ddf582d6bd4496273e6e
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-05-12 23:32:46 +0200

    Updated MatrixMulTest1.

--- test/MatrixMulTest1.hlsl
+++ test/MatrixMulTest1.hlsl
@@ -10,10 +10,10 @@ cbuffer Matrices {
 
 void main(float3 v3 : VEC3, float4 v4 : VEC4) {
 	
-	float3x4 a = mul(M3, M4);
+	float3x4 a = (float3x4)0;
 	float4x4 b = mul(M4, M4);
 	float4x4 c = mul(M4x3, a);
-	
+    
 	float3 d = mul(M3, v3);
 	float4 e = mul(M4, v3);
 	float3 f = mul(M3, v4);
@@ -23,6 +23,11 @@ void main(float3 v3 : VEC3, float4 v4 : VEC4) {
 	float i = mul(v3, v4);
 	float j = mul(v4, v3);
 	float k = mul(v4, v4);
+    
+    float4x4 l = M4*1;
+    float4x4 m = 2*M4;
+    float4x4 n = M4+3;
+    float4x4 o = 4+M4;
 	
 }
 
