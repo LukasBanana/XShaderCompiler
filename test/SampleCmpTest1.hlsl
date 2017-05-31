c9f978654acc3984af865c2714ff8742237e5295
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-05-31 15:51:56 +0200

    Adjusted SampleCmpTest1 for better output comparison.

--- test/SampleCmpTest1.hlsl
+++ test/SampleCmpTest1.hlsl
@@ -5,12 +5,12 @@ SamplerComparisonState ss;
 
 float4 main() : SV_Position
 {
-	float f1 = tex1.SampleCmp(ss, float2(0, 0), 0.5f);
-	float f2 = tex1.SampleCmp(ss, float2(0, 0), 0.5f, int2(3, -3));
-	float f3 = tex2.SampleCmp(ss, float4(0, 0, 0, 0), 0.5f);
+	float f1 = tex1.SampleCmp(ss, float2(1, 2), 0.5f);
+	float f2 = tex1.SampleCmp(ss, float2(1, 2), 0.5f, int2(3, -3));
+	float f3 = tex2.SampleCmp(ss, float4(1, 2, 3, 4), 0.5f);
 	
-	float f4 = tex1.SampleCmpLevelZero(ss, float2(0, 0), 0.5f);
-	float f5 = tex1.SampleCmpLevelZero(ss, float2(0, 0), 0.5f, int2(3, -3));
+	float f4 = tex1.SampleCmpLevelZero(ss, float2(1, 2), 0.5f);
+	float f5 = tex1.SampleCmpLevelZero(ss, float2(1, 2), 0.5f, int2(3, -3));
 	
     return f1 + f2 + f3 + f4 + f5;
 } 
