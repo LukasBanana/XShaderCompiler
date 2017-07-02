645fd04079e15d13b90d7e1835e7fedeaf5f23cb
Lukas Hermanns <lukas.hermanns90@gmail.com>
2017-07-02 16:34:11 +0200

    Started with GLSLScanner.

---
+++ src/Compiler/Frontend/GLSL/GLSLScanner.cpp
@@ -0,0 +1,50 @@
+/*
+ * GLSLScanner.cpp
+ * 
+ * This file is part of the XShaderCompiler project (Copyright (c) 2014-2017 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#include "GLSLScanner.h"
+#include "GLSLKeywords.h"
+#include "ReportIdents.h"
+
+
+namespace Xsc
+{
+
+
+GLSLScanner::GLSLScanner(Log* log) :
+    SLScanner { log }
+{
+}
+
+
+/*
+ * ======= Private: =======
+ */
+
+TokenPtr GLSLScanner::ScanIdentifierOrKeyword(std::string&& spell)
+{
+    /* Scan reserved words */
+    auto it = GLSLKeywords().find(spell);
+    if (it != GLSLKeywords().end())
+    {
+        if (it->second == Token::Types::Reserved)
+            Error(R_KeywordReservedForFutureUse(spell));
+        else if (it->second == Token::Types::Unsupported)
+            Error(R_KeywordNotSupportedYet(spell));
+        else
+            return Make(it->second, spell);
+    }
+
+    /* Return as identifier */
+    return Make(Tokens::Ident, spell);
+}
+
+
+} // /namespace Xsc
+
+
+
+// ================================================================================
---
+++ src/Compiler/Frontend/GLSL/GLSLScanner.h
@@ -0,0 +1,41 @@
+/*
+ * GLSLScanner.h
+ * 
+ * This file is part of the XShaderCompiler project (Copyright (c) 2014-2017 by Lukas Hermanns)
+ * See "LICENSE.txt" for license information.
+ */
+
+#ifndef XSC_GLSL_SCANNER_H
+#define XSC_GLSL_SCANNER_H
+
+
+#include "SLScanner.h"
+
+
+namespace Xsc
+{
+
+
+// GLSL token scanner.
+class GLSLScanner : public SLScanner
+{
+    
+    public:
+        
+        GLSLScanner(Log* log = nullptr);
+
+    private:
+
+        TokenPtr ScanIdentifierOrKeyword(std::string&& spell) override;
+
+};
+
+
+} // /namespace Xsc
+
+
+#endif
+
+
+
+// ================================================================================
