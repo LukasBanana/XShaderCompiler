fea14345a1d8d769d12b0093b8c74d9853892f5d
Lukas Hermanns <lukas.hermanns90@gmail.com>
2018-08-04 11:48:33 +0200

    Updated C# wrapper for new reflection interface.

--- src/Compiler/Xsc.cpp
+++ src/Compiler/Xsc.cpp
@@ -20,8 +20,10 @@ namespace Xsc
 
 
 XSC_EXPORT bool CompileShader(
-    const ShaderInput& inputDesc, const ShaderOutput& outputDesc,
-    Log* log, Reflection::ReflectionData* reflectionData)
+    const ShaderInput&          inputDesc,
+    const ShaderOutput&         outputDesc,
+    Log*                        log,
+    Reflection::ReflectionData* reflectionData)
 {
     /* Compile shader with compiler driver */
     Compiler::StageTimePoints timePoints;
@@ -34,28 +36,6 @@ XSC_EXPORT bool CompileShader(
         reflectionData,
         &timePoints
     );
-
-    #if 0
-    if (reflectionData)
-    {
-        /* Sort reflection */
-        auto SortStats = [](std::vector<Reflection::BindingSlot>& objects)
-        {
-            std::sort(
-                objects.begin(), objects.end(),
-                [](const Reflection::BindingSlot& lhs, const Reflection::BindingSlot& rhs)
-                {
-                    return (lhs.location < rhs.location);
-                }
-            );
-        };
-
-        SortStats(reflectionData->textures);
-        SortStats(reflectionData->constantBuffers);
-        SortStats(reflectionData->inputAttributes);
-        SortStats(reflectionData->outputAttributes);
-    }
-    #endif
 
     /* Show timings */
     if (outputDesc.options.showTimes && log)
@@ -95,7 +75,9 @@ XSC_EXPORT bool CompileShader(
 }
 
 XSC_EXPORT void DisassembleShader(
-    std::istream& streamIn, std::ostream& streamOut, const AssemblyDescriptor& desc)
+    std::istream&               streamIn,
+    std::ostream&               streamOut,
+    const AssemblyDescriptor&   desc)
 {
     switch (desc.intermediateLanguage)
     {
@@ -109,7 +91,7 @@ XSC_EXPORT void DisassembleShader(
             disassembler.Print(streamOut, desc);
 
             #else
-            
+
             throw std::invalid_argument(R_NotBuildWithSPIRV);
 
             #endif
