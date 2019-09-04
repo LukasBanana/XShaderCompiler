ef8af06b595b3703fc6c85d059b3cf429447a42b
Martín Lombardo <mlomb@users.noreply.github.com>
2019-09-04 14:58:56 -0300

    Enable OES_standard_derivatives if a call to fwidth/dFdx/dFdy is present
    Also removed the transformation fwidth -> abs(dFdx)+abs(dFdy)
    
    Note: AcquireExtension is not called because it checks against GLSL versions and not ESSL versions. In the long run it should be moved to a ESSLExtensionAgent.

--- src/Compiler/Backend/GLSL/GLSLExtensionAgent.cpp
+++ src/Compiler/Backend/GLSL/GLSLExtensionAgent.cpp
@@ -312,6 +312,16 @@ IMPLEMENT_VISIT_PROC(CallExpr)
     /* Check for special intrinsics */
     if (ast->intrinsic != Intrinsic::Undefined)
     {
+        if (targetGLSLVersion_ == OutputShaderVersion::ESSL100) {
+            if (ast->intrinsic == Intrinsic::FWidth ||
+                ast->intrinsic == Intrinsic::DDX ||
+                ast->intrinsic == Intrinsic::DDY) {
+                extensions_.insert(E_GL_OES_standard_derivatives);
+                if (onReportExtension_)
+                    onReportExtension_(R_GLSLExtensionAcquired(E_GL_OES_standard_derivatives, ToString(OutputShaderVersion::ESSL100), R_Intrinsic(ast->ident)), ast);
+            }
+        }
+
         auto it = intrinsicExtMap_.find(ast->intrinsic);
         if (it != intrinsicExtMap_.end())
             AcquireExtension(it->second, R_Intrinsic(ast->ident), ast);
